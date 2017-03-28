dbcc0c13b069339a3416296ab1afa39be5e1cf51
Cesar Daniel Perez <cesdperez@gmail.com>
2017-03-27 21:55:05 -0300

    Some tree problems

---
+++ jjava/trees/TreePreorderTraversal.java
@@ -0,0 +1,30 @@
+package jjava.trees;
+
+/**
+ * https://www.hackerrank.com/challenges/tree-preorder-traversal
+ */
+
+public class TreePreorderTraversal {
+
+    class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    void preOrder(Node root) {
+
+        if (root == null) return;
+
+        print(root);
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    void print(Node root) {
+        System.out.print(root.data + " ");
+    }
+
+
+}
